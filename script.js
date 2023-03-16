744a5247dab1ad163adab1cbe4aefac2d146729e
Adrien Turcek-Perron <adrien141094@gmail.com>
2023-03-16 11:24:04 +0100

    name-chalk

---
+++ script.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+console.log("Hello Wilders")
+
+console.log((chalk.blue('Camille')));
+console.log((chalk.green('Victor')));
+console.log((chalk.red('Benoit')));
+console.log((chalk.yellow('Ugo')));
+console.log((chalk.bgBlue('Ludovic')));
